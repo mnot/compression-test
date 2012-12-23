871b3c9370a32f64959d76427cc5cea7217b7408
Mark Nottingham <mnot@mnot.net>
2012-12-23 15:39:21 +1100

    put decompress in http1

--- compression/http1/__init__.py
+++ compression/http1/__init__.py
@@ -2,8 +2,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from .. import BaseProcessor, spdy_dictionary, format_http1
+from .. import BaseProcessor, spdy_dictionary, format_http1, parse_http1
 
 class Processor(BaseProcessor):
   def compress(self, in_headers, host):
     return format_http1(in_headers)
+
+  def decompress(self, compressed):
+    return parse_http1(compressed)
